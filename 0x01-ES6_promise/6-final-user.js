6e0bca2e2a7c6f4915f01523ebc2ae8bf606b250
anne <annemachinda@gmail.com>
2022-07-28 04:07:22 +0100

    user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,7 @@
-import { signUpUser, uploadPhoto } from './5-photo-reject';
-export default function handleProfileSignup() {
-  return Promise.all([(), createUser()])
-    .then((value) => {
-      const { body } = value[0];
-      const { firstName } = value[1];
-      const { lastName } = value[1];
-      console.log(`${body} ${firstName} ${lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.resolve({
+	signUpUser(),
+	uploadPhoto()});
 }
