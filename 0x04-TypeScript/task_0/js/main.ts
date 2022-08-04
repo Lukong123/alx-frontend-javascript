b77746927bdcf4167969e31246326992e0a2d066
anne <annemachinda@gmail.com>
2022-08-04 04:01:01 +0100

    Revert "teacher interface"
    This reverts commit 1788b0677ab12e6ab214fb7162f989679ad23e3a.

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,18 +1,43 @@
-interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
     location: string;
-    [key: string]: any;
   }
 
-  const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-  };
-  
-  console.log(teacher3);
+const student1: Student = {
+    firstName: "Anne",
+    lastName: "Berinyuy",
+    age: 20,
+    location: "Bamenda",
+
+};
+
+const student2: Student = {
+    firstName: "Oscar",
+    lastName: "Berinyuy",
+    age: 30,
+    location: "Cameroon",
+
+};
+
+const studentsList: Student[] = [student1, student2];
+console.log(studentsList);
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentsList.forEach((student: Student): void => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row)
+
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
