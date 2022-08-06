9a743d7ec9a129b72efe729452c4d6e39eb82545
anne <annemachinda@gmail.com>
2022-08-06 15:01:33 +0100

    adv

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -41,4 +41,17 @@ class Teacher implements TeacherInterface{
 function createEmployee(salary: string | number): Teacher | Director {
     if (typeof salary === 'number' && salary < 500) return new Teacher();
     return new Director;
-} 
+}
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else {
+        return employee.workTeacherTasks();
+    }
+}
