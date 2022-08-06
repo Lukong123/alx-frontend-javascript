7ad0ae338dd3fb93f58abf549723bbc8e6ffccb5
anne <annemachinda@gmail.com>
2022-08-06 14:08:02 +0100

    director print teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,18 @@ interface Teacher {
 interface Directors extends Teacher{
     numberOfReports: number;
 }
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string =>`${firstName[0]}. ${lastName}`;
