ec6546397bd1ab216b643b0142251881262d5450
anne <annemachinda@gmail.com>
2022-08-06 13:53:55 +0100

    js

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,12 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [attribute: string]: any;
+  }
+
+interface Directors extends Teacher{
+    numberOfReports: number;
+}
